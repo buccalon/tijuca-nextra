4c8655caa5c9de4953d11eb37490d4fbddb0b11c
Bruno Buccalon <buccalon@users.noreply.github.com>
2022-04-10 11:07:03 -0500

    Add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div class='docs-container relative pt-16 pb-16 px-6 md:px-8 w-full max-w-full min-w-0'>
+      <h1>Página não encontrada</h1>
+      <p><a href="./">← Retornar ao <strong>tiju.ca</strong></a></p>
+    </div>
+  )
+}
