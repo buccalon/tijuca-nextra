0ffc8c1e122e896dbd81abf3c1dc326c057a174f
Bruno Buccalon <buccalon@users.noreply.github.com>
2022-09-10 19:46:07 -0500

    Fix build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.tsx',
-    unstable_staticImage: true,
-    unstable_flexsearch: {
-        codeblocks: true
-    }
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  unstable_staticImage: true,
+  unstable_flexsearch: {
+    codeblock: false
+  }
 })
 
 module.exports = withNextra({
-    reactStrictMode: true
+  reactStrictMode: true
 })
